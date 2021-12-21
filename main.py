2b7cad984b8c854022f1dd9de4a52fea1cd7cf52
Dushes4 <85163946+Dushes4@users.noreply.github.com>
2021-12-21 13:41:16 +0300

    Posts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -9,6 +10,7 @@ db = SQLAlchemy(app)
 
 
 class Post(db.Model):
+    title = db.Column(db.String(300))
     post_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
     picture_link = db.Column(db.String(300), nullable=False)
@@ -19,29 +21,60 @@ class Post(db.Model):
         return '<Post %r>' % self.post_id
 
 
+class User_auth(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+
+
 @app.route('/')
-def index():
-    return render_template("index.html")
+@app.route('/home')
+def home():
+    posts = Post.query.order_by(Post.post_id.desc()).all()
+    return render_template("home.html", posts=posts)
 
 
-@app.route('/user/<int:user_id>')
+@app.route('/new_post', methods=['POST', 'GET'])
+def new_post():
+    if request.method == "POST":
+
+        title = request.form['title']
+        picture_link = request.form['picture_link']
+
+        try:
+            last_post_id = Post.query.order_by(Post.post_id.desc()).first().post_id
+        except:
+            last_post_id = 0
+
+        try:
+            if (requests.get(picture_link).headers['Content-Type']) in ('image/gif', 'image/png', 'image/jpg', 'image/jpeg'):
+                new_post_db = Post(title=title, picture_link=picture_link, user_id=1, post_id=last_post_id + 1, likes=0)
+                db.session.add(new_post_db)
+                db.session.commit()
+            else:
+                return "Error"
+        except:
+            return "Error"
+        return redirect('/home')
+
+    else:
+        return render_template("new_post.html")
+
+
+@app.route('/user')
 def user():
-    return render_template("user_profile.html")
+    return render_template("Profile.html")
 
 
 @app.route('/login')
 def login():
-    return "Welcome to login page"
+    return render_template("Log-in.html")
 
 
 @app.route('/register')
 def register():
-    return "Welcome to register page"
-
-
-@app.route('/new_post')
-def new_post():
-    return "New post creation"
+    return render_template("Sign-up.html")
 
 
 if __name__ == "__main__":
